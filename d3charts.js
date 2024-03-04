5e4d39fe4b609132c3292baaa1adb9fa00a5383d
youssef819 <90600113+youssef819@users.noreply.github.com>
2024-03-04 08:29:51 +0100

    init 2

--- d3charts.js
+++ d3charts.js
@@ -309,7 +309,8 @@ function getPitchLines() {
   }
 
   function get_all_team_matchs_data(goals,players,data,team,season) {
-
+    if(data!="False"){
+    console.log(data)
     var selected_season=data.filter(row => (row[1] === season ))
     var selected_team=selected_season.filter(row => (row[3] == team || row[4] == team))
     if(selected_team.length===0){
@@ -353,7 +354,10 @@ function getPitchLines() {
     }
     updatedData=updatedData.filter(row => (row[14] == team));
 
-    return updatedData.slice(1)
+    return updatedData.slice(1)}
+    else{
+      return "Team did not participate in this season"
+    }
   }
   
 
@@ -373,19 +377,19 @@ function getTeamsByOutcome(outcome) {
 
     // Parcourez les matchs et ajoutez les équipes correspondantes en fonction du résultat
     manCityMatches.forEach(match => {
-        if (outcome === 'Matchs gagnés') {
+        if (outcome === 'Wins') {
             if (match['3'] === Team && match['6'] > match['7']) {
                 teams.push(match['4']);
             } else if (match['4'] === Team && match['7'] > match['6']) {
                 teams.push(match['3']);
             }
-        } else if (outcome === 'Matchs perdus') {
+        } else if (outcome === 'Loses') {
             if (match['3'] === Team && match['6'] < match['7']) {
                 teams.push(match['4']);
             } else if (match['4'] === Team && match['7'] < match['6']) {
                 teams.push(match['3']);
             }
-        } else if (outcome === 'Matchs nuls') {
+        } else if (outcome === 'Draws') {
             if (match['6'] === match['7']) {
                 if (match['3'] === Team) {
                     teams.push(match['4']);
@@ -424,9 +428,9 @@ manCityMatches.forEach(match => {
 
 // Données pour le pie chart
 const pieChartData = [
-    { label: 'Matchs nuls', value: draws },
-    { label: 'Matchs gagnés', value: wins },
-    { label: 'Matchs perdus', value: losses }
+    { label: 'Draws', value: draws },
+    { label: 'Wins', value: wins },
+    { label: 'Loses', value: losses }
 ];
 
 // Dimensions et paramètres du pie chart
@@ -434,7 +438,7 @@ const width = 300;
 const height = 300;
 const radius = Math.min(width, height) / 2;
 const color = d3.scaleOrdinal(d3.schemeCategory10);
-const margin = ({top: 0, right: 50, bottom: 0, left: 150})
+const margin = ({top: 150, right: 50, bottom: 0, left: 150})
 
 var container = d3.select(container)
 var svg = container.select("svg");
@@ -512,9 +516,12 @@ const arcs = g.selectAll('arc')
         // Masquer la bulle d'information lorsque le curseur quitte la tranche
         tooltip.style('display', 'none');
     });
+
+    const newColorScale = d3.scaleOrdinal()
+    .range(["#264b96", "#1ab862", "#bf212f"]);    
 arcs.append('path')
     .attr('d', arc)
-    .attr('fill', d => color(d.data.label));
+    .attr('fill', d => newColorScale(d.data.label));
 
 // Créer des masques pour chaque tranche du pie chart
 const defs = svg.append('defs');
@@ -529,22 +536,6 @@ const defs = svg.append('defs');
     //.attr('fill', 'black');
 
 // Ajouter des images aux tranches du pie chart
-arcs.append('image')
-    .attr('xlink:href', d => {
-        // Insérez les liens vers les images correspondant à chaque catégorie (matchs nuls, gagnés et perdus)
-        if (d.data.label === 'Matchs nuls') {
-            return '';
-        } else if (d.data.label === 'Matchs gagnés') {
-            return '';
-        } else if (d.data.label === 'Matchs perdus') {
-            return '';
-        }
-    })
-    .attr('width', 421)
-    .attr('height', 402)
-    .attr('mask', (_, i) => `url(#mask-${i})`)
-    .attr('transform', d => `translate(${arc.centroid(d)})`)
-    .style('pointer-events', 'none'); // Assurez-vous que les images ne captent pas les événements de souris pour que les événements de survol fonctionnent correctement
 
 // Ajouter les labels
 arcs.append('text')
@@ -572,214 +563,174 @@ return svg.node();
 }
 
 
-function piechart2(matches, Season, Team,container,tooltip){
-
- 
 
 
-
-  // Filtrer les matchs impliquant Manchester City pour une saison donnée
-  const manCityMatches = matches.filter(match => (match['3'] === Team || match['4'] === Team) && match['1'] === Season);
-  function getTeamsByOutcome(outcome) {
-      // Créez un tableau vide pour stocker les noms d'équipe correspondants
-      const teams = [];
-  
-      // Parcourez les matchs et ajoutez les équipes correspondantes en fonction du résultat
-      manCityMatches.forEach(match => {
-          if (outcome === 'Matchs gagnés') {
-              if (match['3'] === Team && match['6'] > match['7']) {
-                  teams.push(match['4']);
-              } else if (match['4'] === Team && match['7'] > match['6']) {
-                  teams.push(match['3']);
-              }
-          } else if (outcome === 'Matchs perdus') {
-              if (match['3'] === Team && match['6'] < match['7']) {
-                  teams.push(match['4']);
-              } else if (match['4'] === Team && match['7'] < match['6']) {
-                  teams.push(match['3']);
-              }
-          } else if (outcome === 'Matchs nuls') {
-              if (match['6'] === match['7']) {
-                  if (match['3'] === Team) {
-                      teams.push(match['4']);
-                  } else if (match['4'] === Team) {
-                      teams.push(match['3']);
-                  }
-              }
-          }
-      });
-  
-  
-      // Retourne le tableau contenant les noms d'équipe correspondants
-      return teams;
-  }
-  
+function Plot_barchart(dataset,container) {
+var width = 500;
+var height = 400;
+    // Initialiser les données de sortie avec les types de buts
+      var output_data = {
+        "right-footed": { "scored": 0 },
+        "left-footed": { "scored": 0 },
+        "penalty": { "scored": 0 },
+        "own goal": { "scored": 0 },
+        "header": { "scored": 0 },
+        "free kick": { "scored": 0 },
+        "bicycle kick": { "scored": 0 },
+        "other": { "scored": 0 }
+      };
+      console.log(dataset)
+      // Parcourir la dataset pour compter les buts marqués dans chaque type de but
+      dataset.forEach(match => {
+        var goal_type = match[11]; // Type de but
     
-  // Compter le nombre de matchs nuls, gagnés et perdus
-  let draws = 0;
-  let wins = 0;
-  let losses = 0;
-  let non =0;
-  
-  manCityMatches.forEach(match => {
-      if (match['3'] === Team && match['6'] === match['7']) {
-          draws++;
-      } else if (match['4'] === Team && match['6'] === match['7']) {
-          draws++;   
-      } else if (match['3'] === Team && match['6'] > match['7']) {
-          wins++;
-      } else if (match['4'] === Team && match['7'] > match['6']) {
-          wins++;
-      } else {
-          losses++;
-      }
-  });
-  
-  // Données pour le pie chart
-  const pieChartData = [
-      { label: 'Matchs nuls', value: draws },
-      { label: 'Matchs gagnés', value: wins },
-      { label: 'Matchs perdus', value: losses }
-  ];
-  
-  // Dimensions et paramètres du pie chart
-  const width = 300;
-  const height = 300;
-  const radius = Math.min(width, height) / 2;
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
-  const margin = ({top: 0, right: 0 , bottom: 0, left: 450})
-  
-  var container = d3.select(container)
-  var svg = container.select("svg");
-  
-  if (svg.empty()) {
-    // If the SVG doesn't exist, create a new one
-    svg = container.append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .on('mouseleave', () => tooltip.style('display', 'none')); // Cacher la bulle d'information lorsque le curseur quitte le PieChart
-  
-  
-  } else {
-    // If the SVG exists, remove its previous content
-    svg.selectAll("*").remove();
-  
-  }
-  const g = svg.append('g')
-  
-  .attr('transform', `translate(${margin.left},${margin.top})`);  
-  
-  
-  
-  
-  
-  
-  
-  
-  //var container = d3.select(container)
-  
-  //const svg = container.append("svg")
-  //.attr('top', margin.top)
-  //.attr('bottom', margin.bottom)
-   //   .attr('width', width)
-    //  .attr('height', height)
-     // .on('mouseleave', () => tooltip.style('display', 'none')); // Cacher la bulle d'information lorsque le curseur quitte le PieChart
-  //const g = svg.append('g')
-   //   .attr('transform', `translate(${margin.left},${margin.top})`);
-      
-      
-    // Création de l'arc pour chaque tranche de pie chart
-  const arc = d3.arc()
-      .innerRadius(0)
-      .outerRadius(radius);
-  
-  // Création de la fonction pie
-  const pie = d3.pie()
-      .value(d => d.value);
-  
-  // Tracer le pie chart
-  const arcs = g.selectAll('arc')
-      .data(pie(pieChartData))
-      .enter()
-      .append('g')
-      .on('mouseover', function(event, d) {
-          // Réduire l'opacité de toutes les tranches sauf celle survolée
-          arcs.style('opacity', 0.3);
-          d3.select(this).style('opacity', 1);
-  
-          // Afficher la bulle d'information
-          const teams = getTeamsByOutcome(d.data.label);
-          
-          const tooltipContent = teams.map(team => `- ${team}.`).join('\n') + '\n';        
-  
-          tooltip.text(tooltipContent)
-                 .style('display', 'block')
-                 .style('left', `${event.pageX}px`)
-                 .style('top', `${event.pageY}px`)
-                 .style('white-space', 'pre-line');
-      })
-      .on('mouseout', function() {
-          // Restaurer l'opacité normale de toutes les tranches
-          arcs.style('opacity', 1);
-          
-          // Masquer la bulle d'information lorsque le curseur quitte la tranche
-          tooltip.style('display', 'none');
+        // Vérifier le type de but et incrémenter le nombre de buts marqués dans ce type
+        console.log(goal_type)
+
+        switch (goal_type) {
+
+          case "right-footed shot":
+            output_data["right-footed"]["scored"]++;
+            break;
+          case "left-footed shot":
+            output_data["left-footed"]["scored"]++;
+            break;
+          case "penalty":
+            output_data["penalty"]["scored"]++;
+            break;
+          case "own goal":
+            output_data["own goal"]["scored"]++;
+            break;
+          case "header":
+            output_data["header"]["scored"]++;
+            break;
+          case "free kick":
+            output_data["free kick"]["scored"]++;
+            break;
+          case "bicycle kick":
+            output_data["bicycle kick"]["scored"]++;
+            break;
+          default:
+            output_data["other"]["scored"]++;
+        }
       });
-  arcs.append('path')
-      .attr('d', arc)
-      .attr('fill', d => color(d.data.label));
+      console.log(output_data)
+    const unique_symbols = ["scored"];
+    const goals_grouped = d3.rollup(
+      Object.entries(output_data), // Convertir l'objet en tableau
+      entries => unique_symbols.map(key => entries.reduce((acc, [_, value]) => acc + value[key], 0)),
+      ([type]) => type);
+    
+
+
+
+      const margin = ({top: 50, right: 0 , bottom: 0, left: 23})
   
-  // Créer des masques pour chaque tranche du pie chart
-  const defs = svg.append('defs');
-  //const maskGroup = defs.selectAll('mask')
-      //.data(pie(pieChartData))
-      //.enter()
-      //.append('mask')
-      //.attr('id', (_, i) => `mask-${i}`);
-  
-  //maskGroup.append('path')
-      //.attr('d', arc)
-      //.attr('fill', 'black');
-  
-  // Ajouter des images aux tranches du pie chart
-  arcs.append('image')
-      .attr('xlink:href', d => {
-          // Insérez les liens vers les images correspondant à chaque catégorie (matchs nuls, gagnés et perdus)
-          if (d.data.label === 'Matchs nuls') {
-              return '';
-          } else if (d.data.label === 'Matchs gagnés') {
-              return '';
-          } else if (d.data.label === 'Matchs perdus') {
-              return '';
-          }
-      })
-      .attr('width', 421)
-      .attr('height', 402)
-      .attr('mask', (_, i) => `url(#mask-${i})`)
-      .attr('transform', d => `translate(${arc.centroid(d)})`)
-      .style('pointer-events', 'none'); // Assurez-vous que les images ne captent pas les événements de souris pour que les événements de survol fonctionnent correctement
-  
-  // Ajouter les labels
-  arcs.append('text')
-      .attr('transform', d => `translate(${arc.centroid(d)})`)
-      .attr('text-anchor', 'middle')
-      .attr('font-size', '12px')
-      .text(d => `${d.data.label}:\n${d.data.value}`);
+      var container = d3.select(container)
+      var svg = container.select("svg");
       
-  // Création de la bulle d'information (tooltip)
-  var tooltip = svg.append('svg:title')
-     .append('div')
-     .attr('class', 'tooltip')
-     .style('display', 'none')
-     .style('position', 'absolute')
-     .style('background-color', 'black')
-     .style('padding', '10px')
-     .style('border', '3px solid black')
-     .style('color', 'blue');
-  
-  // Ajouter d'autres styles selon vos besoins
-  tooltip.style('color', 'blue')
-         .style('font-size', '12px');
-  
-  return svg.node();
-  }
+      if (svg.empty()) {
+        // If the SVG doesn't exist, create a new one
+        svg = container.append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+      
+      
+      } else {
+        // If the SVG exists, remove its previous content
+        svg.selectAll("*").remove();
+      
+      }
+
+    
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(goals_grouped, ([_, [scored]]) => Math.max(scored))])
+      .range([height - margin.bottom, margin.top]);
+    
+    const x1 = d3.scaleBand()
+      .domain([...goals_grouped.keys()])
+      .padding(.2)
+      .range([margin.left, width - margin.right]);
+    
+    const x2 = d3.scaleBand()
+      .domain(["scored"])
+      .padding(0)
+      .range([0, x1.bandwidth()]);
+    
+    const g = svg.selectAll("g").data(goals_grouped)
+      .enter()
+      .append("g")
+      .attr("transform", d => "translate(" + x1(d[0]) + ", 0)");
+    
+    const c = d3.scaleOrdinal(d3.schemeCategory10).domain(["scored"]);
+    
+    const rect=  g.selectAll("rect")
+    
+
+      .data(d => d[1])
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x2(unique_symbols[i]))
+      .style("fill", "#014421")
+      .attr("y", d => y(d))
+      .attr("width", x2.bandwidth())
+      .attr("height", d => height - y(d) - margin.bottom)
+      .attr("fill", (d, i) => c(unique_symbols[i]))
+      .each(function (d) {
+        // Set initial opacity as a property of each rectangle's data
+        d.initialOpacity = 1;
+      })
+      .on("mouseover", function(event, d) {
+        const label = `${d} ${'scored'}`;
+        tooltip.style("display", "block")
+          .html(label)
+          .style("left", (event.pageX) + "px")
+          .style("top", (event.pageY - 28) + "px");
+          console.log("hello")
+
+          console.log(event.target)
+          rect.style("opacity",0.2)
+          d3.select(this).style("opacity", 1); // Retour à l'opacité par défaut
+
+      })
+      .on("mouseleave", function() {
+        //tooltip.style("display", "none");
+        d3.select(this).style("opacity", 0.2); // Retour à l'opacité par défaut
+        rect.style("opacity",1)
+
+      });
+    const xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x1));
+    
+    const yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+    
+    svg.append("g")
+      .call(xAxis)
+      .selectAll("text") // Sélectionnez tous les éléments texte de l'axe des abscisses
+
+      .style("fill", "white") // Appliquez la couleur noire intense
+      .style("font-weight", "bold");  // Make text bold
+
+    
+    svg.append("g")
+      .call(yAxis)
+      .selectAll("text") // Sélectionnez tous les éléments texte de l'axe des abscisses
+
+      .style("fill", "white") // Appliquez la couleur noire intense
+      .style("font-weight", "bold");  // Make text bold
+    // Création de la bulle d'information (tooltip)
+    const tooltip = svg.append('svg:title')
+          .append('div')
+          .attr('class', 'tooltip')
+          .style('display', 'none')
+          .style('position', 'absolute')
+          .style('background-color', 'white')
+          .style('padding', '5px')
+          .style('border', '1px solid black');
+    
+    return svg.node();
+    
+    }
